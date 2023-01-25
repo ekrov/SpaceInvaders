6469d03a0089c5f25f6daa414f7d619b0726b9d9
ekrov <carlos.antunes.almeida@gmail.com>
2023-01-25 17:27:34 +0000

    test

--- list_ch13_06_pong_text.vhd
+++ list_ch13_06_pong_text.vhd
@@ -16,15 +16,16 @@ end pong_text;
 architecture arch of pong_text is
    signal pix_x, pix_y: unsigned(9 downto 0);
    signal rom_addr: std_logic_vector(10 downto 0);
-   signal char_addr, char_addr_s, char_addr_r,
+   signal char_addr, char_addr_s,char_addr_s_2p, char_addr_r,
           char_addr_o: std_logic_vector(6 downto 0);
-   signal row_addr, row_addr_s, row_addr_r,
+   signal row_addr, row_addr_s,row_addr_s_2p, row_addr_r,
           row_addr_o: std_logic_vector(3 downto 0);
-   signal bit_addr, bit_addr_s, bit_addr_r,
+   signal bit_addr, bit_addr_s,bit_addr_s_2p, bit_addr_r,
           bit_addr_o: std_logic_vector(2 downto 0);
    signal font_word: std_logic_vector(7 downto 0);
    signal font_bit: std_logic;
-   signal score_on, rule_on, over_on: std_logic;
+   signal score_on,score_2p_on, rule_on, over_on: std_logic;
+
    signal rule_rom_addr: unsigned(6 downto 0);
    type rule_rom_type is array (0 to 95) of
        std_logic_vector (6 downto 0);
@@ -172,6 +173,37 @@ begin
         "1110011" when "1101", -- s x73
         "0111010" when "1110", -- :
         "01100" & ball when others;
+
+        ---------------------------------------------
+   -- score region
+   --  - display two-digit score, ball on top left
+   --  - scale to 16-by-32 font
+   --  - line 1, 16 chars: "Score:DD Ball:D"
+   ---------------------------------------------
+   score_2p_on <=
+      '1' when pix_y(9 downto 5)=20 and
+               pix_x(9 downto 4)<16 else
+      '0';
+   row_addr_s_2p <= std_logic_vector(pix_y(4 downto 1));
+   bit_addr_s_2p <= std_logic_vector(pix_x(3 downto 1));
+   with pix_x(7 downto 4) select
+     char_addr_s_2p <=
+        "1010011" when "0000", -- S x53
+        "1100011" when "0001", -- c x63
+        "1101111" when "0010", -- o x6f
+        "1110010" when "0011", -- r x72
+        "1100101" when "0100", -- e x65
+        "0111010" when "0101", -- : x3a
+        "011" & dig1 when "0110", -- digit 10
+        "011" & dig0 when "0111", -- digit 1
+        "0000000" when "1000",
+        "1001100" when "1001", -- L x4c
+        "1101001" when "1010", -- i x69
+        "1110110" when "1011", -- v x76
+        "1100101" when "1100", -- e x65
+        "1110011" when "1101", -- s x73
+        "0111010" when "1110", -- :
+        "01100" & ball when others;
    ---------------------------------------------
    -- rule region
    --   - display rule (4-by-16 tiles)on center
@@ -213,16 +245,23 @@ begin
    ---------------------------------------------
    -- mux for font ROM addresses and rgb
    ---------------------------------------------
-   process(score_on,rule_on,pix_x,pix_y,font_bit,
-           char_addr_s,char_addr_r,char_addr_o,
-           row_addr_s,row_addr_r,row_addr_o,
-           bit_addr_s,bit_addr_r,bit_addr_o)
+   process(score_on,score_2p_on,rule_on,pix_x,pix_y,font_bit,
+           char_addr_s,char_addr_s_2p,char_addr_r,char_addr_o,
+           row_addr_s,row_addr_s_2p,row_addr_r,row_addr_o,
+           bit_addr_s,bit_addr_s_2p,bit_addr_r,bit_addr_o)
    begin
       text_rgb <= "000";  -- background, black
       if score_on='1' then
          char_addr <= char_addr_s;
          row_addr <= row_addr_s;
          bit_addr <= bit_addr_s;
+         if font_bit='1' then
+            text_rgb <= "111";
+         end if;
+      elsif score_2p_on='1' then
+         char_addr <= char_addr_s_2p;
+         row_addr <= row_addr_s_2p;
+         bit_addr <= bit_addr_s_2p;
          if font_bit='1' then
             text_rgb <= "111";
          end if;
